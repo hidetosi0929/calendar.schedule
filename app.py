9eeb1b5d3e7a368a3863467f51e7850e9ad3081c
hidetosi0929 <hidetosi.isii@gmail.com>
2025-09-06 10:38:24 +0900

    カレンダー

--- app.py
+++ app.py
@@ -2,24 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-events = [
-    {
-        'title': 'event1',
-        'date': '2025-9-15'
-    },
-    {
-        'title': 'event2',
-        'date': '2025-9-20'
-    }
-]
+events = [{"title": "event1", "date": "2025-9-15"}, {"title": "event2", "date": "2025-9-20"}]
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/calendar')
+
+@app.route("/calendar")
 def calendar():
-    return render_template('calendar.html', events=events)
+    return render_template("calendar.html", events=events)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
